c783cba9e628d7709b90c53b3ed35388589a765b
Thomas Davis <thomasalwyndavis@gmail.com>
2020-09-13 16:37:40 +1000

    Asd

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,46 @@
 import React, { Component } from "react";
 import _ from "lodash";
 import axios from "axios";
+import styled from "styled-components";
+
+const Title = styled.h1``;
+
+const Container = styled.div`
+  font-family: "Open Sans", sans-serif;
+  max-width: 400px;
+  margin: auto;
+`;
+
+const Dropdown = styled.select``;
+
+const Byline = styled.div``;
+
+const TextInput = styled.textarea`
+  width: 100%;
+  padding: 10px;
+  outline: 0;
+  border: 1px solid #ebebeb;
+`;
+const LanguageContainer = styled.div`
+  display: flex;
+  padding-bottom: 15px;
+  justify-content: space-between;
+`;
+const DictionaryLink = styled.a`
+  color: #000;
+  text-decoration: none;
+  font-weight: bold;
+  &::hover {
+    color: red;
+  }
+  &::visited {
+    color: #333;
+  }
+`;
+const Footer = styled.div`
+  font-size: 12px;
+  color: #555;
+`;
 class About extends Component {
   constructor(props) {
     super(props);
@@ -46,15 +86,51 @@ class About extends Component {
   render() {
     const { text, translatedText } = this.state;
     return (
-      <div>
-        <h3>How you speaka</h3>
-        <textarea onChange={this.onTextChange}>{text}</textarea>
+      <Container>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+        <Title>MOB TRANSLATE</Title>
+        <Byline>Google translate for Australian Aboriginal languages</Byline>
+        <br />
+        <LanguageContainer>
+          <Dropdown>
+            {" "}
+            <option> Kuku Yalanji</option>
+          </Dropdown>
+          <DictionaryLink href="https://api.mobtranslate.com/">
+            Dictionary
+          </DictionaryLink>
+        </LanguageContainer>
+        <TextInput
+          placeholder="Enter text e.g. I'm a woman who likes good food"
+          onChange={this.onTextChange}
+        >
+          {text}
+        </TextInput>
         <br />
         <br />
-        <h3>Translated</h3>
+        <h3>Translation</h3>
+        <TextInput
+          onChange={this.onTextChange}
+          disabled={true}
+          value={translatedText}
+        >
+          {translatedText}
+        </TextInput>
         <br />
-        <p>{translatedText}</p>
-      </div>
+        <br />
+        <Footer>
+          MOB TRANSLATE is a fully open source and free project.
+          <br />
+          <br />
+          Currently in development and looking for help.
+          <br />
+          <br />
+          In memory of Tuan Coolwell
+        </Footer>
+      </Container>
     );
   }
 }
